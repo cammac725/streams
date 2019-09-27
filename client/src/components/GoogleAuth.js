9df3c6ecd549ca6985871b25174c6c7cb6cf2aa6
cammac <camm@rocktechcomputer.com>
2019-09-27 14:21:20 -0700

    scaffolding app, setup OAuth

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+
+class GoogleAuth extends Component {
+    state = { isSignedIn: null }
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId:
+                    '1031989405287-rf74264h51usk1omkcn5i4fr3v1utf5p.apps.googleusercontent.com',
+                scope: 'email'
+            })
+            .then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance()
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.auth.isSignedIn.listen(this.onAuthChange)
+            })
+        })
+    }
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    }
+
+    onSignIn = () => {
+        this.auth.signIn()
+    }
+
+    onSignOut = () => {
+        this.auth.signOut()
+    }
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return null
+        } else if (this.state.isSignedIn){
+            return (
+                <button className="ui red google button">
+                    <i className='google icon' />
+                    Sign Out
+                </button>
+            ) 
+        } else {
+            return (
+                <button className="ui red google button">
+                    <i className='google icon' />
+                    Sign In with Google
+                </button>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                {this.renderAuthButton()}
+            </div>
+        )
+    }
+}
+
+export default GoogleAuth
