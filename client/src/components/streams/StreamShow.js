08a4cc176a50196e4a3d8b245cd3f92e50b39f09
cammac <camm@rocktechcomputer.com>
2019-10-22 17:04:04 -0700

    finalized streaming app, cwu

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -19,8 +19,11 @@ class StreamShow extends React.Component {
     componentDidUpdate() {
         this.buildPlayer()
     }
-    
 
+    componentWillUnmount() {
+        this.player.destroy()
+    }
+    
     buildPlayer() {
         if (this.player || !this.props.stream) {
             return
